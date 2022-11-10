e8f48e163ad018776d3c7e776a854d2815f60de8
khamdullaevuz <elbek.khamdullaev@gmail.com>
2022-11-10 10:21:35 +0500

    add amount to success

--- action.php
+++ action.php
@@ -39,7 +39,7 @@ if ($_GET['method'] == 'create') {
             if ($value['sum']['amount'] == $info['amount']) {
                 $db->query("UPDATE `qiwi_log` SET `is_completed` = '1' WHERE `id` = '$id'");
                 $db->query("UPDATE `bot_users` SET `modeys` = `modeys` + '{$info['amount']}' WHERE `user_id` = '{$info['user_id']}'");
-                echo json_encode(['ok' => true, 'status' => 'success']);
+                echo json_encode(['ok' => true, 'status' => 'success', 'amount' => $info['amount']]);
                 exit;
             } else {
                 echo json_encode(['ok' => false, 'status' => 'error']);
